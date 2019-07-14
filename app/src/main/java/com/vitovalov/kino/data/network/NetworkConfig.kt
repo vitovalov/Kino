5f3741245fb8ab50e44439e03c07c7eac4161e0a
Vito Valov <vitovalov@gmail.com>
2019-07-14 17:39:49 +0200

    Api key

--- app/src/main/java/com/vitovalov/kino/data/network/NetworkConfig.kt
+++ app/src/main/java/com/vitovalov/kino/data/network/NetworkConfig.kt
@@ -4,7 +4,7 @@ import java.util.*
 
 object NetworkConfig {
     const val API_URL = "https://api.themoviedb.org/3/"
-    const val API_KEY = "***REMOVED***"
+    const val API_KEY = "5d9"+"67c7c3"+"35764f"+"39b1efb"+"e9c5de9760"
     val API_LANG: String
         get() = Locale.getDefault().language
     const val API_BASE_IMAGE = "https://image.tmdb.org/t/p/"
